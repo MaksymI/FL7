ffc9cf69af785f668416e27e6004c69f36a746cb
MaksymI <maksym.izmailov.lv@gmail.com>
2017-07-14 23:41:30 +0300

    play after first fail

--- 09_Control_flow/hw/task2/script.js
+++ 09_Control_flow/hw/task2/script.js
@@ -1,26 +1,27 @@
 var game = confirm ("Чи бажаєте почати гру?");
-var win_base_1 = 10;
-var win_base_2 = 5;
-var win_base_3 = 2;
-var win_all = 0;
-var win_multiplayer = 1;
 
-var range_base = 5;
-var range_multiplayer = 1;
-
-var num_of_attempts = 3;
-var win_amount = 0;
 
 if (!game) {
     console.log("Сьогодні ви не виграли мільйон, а могли");
 } else {
+    var win_base_1 = 10;
+    var win_base_2 = 5;
+    var win_base_3 = 2;
+    var win_all = 0;
+    var win_multiplayer = 1;
+
+    var range_base = 5;
+    var range_multiplayer = 1;
+
+    var num_of_attempts = 3;
+    var win_amount = 0;
 
     outer: while (game) {
      var number = Math.floor( Math.random() * (range_base * range_multiplayer + 1) );
      console.log(`hint ;) - ${number}`); // for test purpose. Must be deleted on production 
     
 
-        while (num_of_attempts--) {
+       inner: while (num_of_attempts--) {
             var gamer_number = prompt(`Вгадайте число в проміжку від 0 до ${range_base*range_multiplayer}`);
          
             if (gamer_number == number) {
@@ -45,7 +46,7 @@ if (!game) {
                 break;
             } 
             
-            // якщо це не перша гра (а гра на підвищення ставок - range_multiplayer != 1) та гравець програв ((num_of_attempts === 0)
+            // якщо це не перша гра (а гра на підвищення ставок - range_multiplayer != 1) та гравець програв (num_of_attempts === 0)
             else if (num_of_attempts === 0 && range_multiplayer != 1) {
                     win_all = 0;
                     win_amount = 0;
@@ -54,20 +55,20 @@ if (!game) {
                     break outer;
                 }
 
-            
-
         }
 
         game = confirm (`Ваш виграш - ${win_amount}$. Чи бажаєте продовжити гру?`);
 
         if (!game) {
             console.log(`Дякуємо за гру, ваш виграш становить - ${win_amount}$`);
+        } else if (range_multiplayer === 1 && win_amount === 0 ) { // якщо це перша гра та гравець програв, але хоче грати ще раз (game=true)
+                num_of_attempts = 3;
+                 // просто продовжуємо гру. не збільшуємо range_multiplayer, win_multiplayer та win_all
+        } else { // якщо гравець виграв та грає далі на підвищення
+            win_all += win_amount;
+            range_multiplayer = range_multiplayer * 2;
+            win_multiplayer = win_multiplayer * 3;
+            num_of_attempts = 3;
         }
-
-        win_all += win_amount;
-        range_multiplayer = range_multiplayer * 2;
-        win_multiplayer = win_multiplayer * 3;
-        num_of_attempts = 3;
-
     }
 }
