8d9800732a0ab760b6b2e42742e4086918726175
MaksymI <maksym.izmailov.lv@gmail.com>
2017-07-15 17:28:00 +0300

    L10 Homework functions part

--- 10_Functions/hw/task1/script.js
+++ 10_Functions/hw/task1/script.js
@@ -1,12 +1,76 @@
-var sh = '#';
-var sp = ' ';
+// isBigger - a function which accepts two arguments and returns true if first one
+// has greater value than second one or false otherwise.  
+// E.g: isBigger(5, -1); // => true
+// Tip: no need for if/else clause nor ternary operator
 
-var i = 1;
-while (i < 9) {
-    if (i % 2 === 0) {
-        console.log(`${sp}${sh}${sp}${sh}${sp}${sh}${sp}${sh}`);
-    } else {
-        console.log(`${sh}${sp}${sh}${sp}${sh}${sp}${sh}`);
-    }
-    i++;
+function isBigger (a, b) {
+    return a>b;
 }
+
+console.log(isBigger(5, -1));
+
+
+// isSmaller - a function which accepts two arguments and returns true if first one
+// has lesser value than second one or false otherwise.
+// E.g. isSmaller(5, -1); // => false
+// Tip: consider reusing isBigger function
+
+function isSmaller (a, b) {
+    return !isBigger(a, b);
+}
+
+console.log(isSmaller(5, -1));
+
+
+// getMin - a function which accepts arbitrary number of integer arguments
+// and returns the one with the smallest value.
+// E.g. getMin(3, 0, -3); // => -3
+// Tip: since arguments is like array, you can use simple iteration over it
+// and use arguments[ i ] to get the argument of a given index
+
+function getMin () {
+    var min = arguments[0];
+    for (let i = 1; i<arguments.length; i++) {
+        if (arguments[i] < min) {
+            min = arguments[i];
+        }
+    }
+    return min;
+}
+
+console.log(getMin(3, 0, -3));
+
+
+// isPrime - a function which accepts one integer argument and returns true
+// if it is prime number or false otherwise
+
+function isPrime (num) {
+    var prime = true;
+	for(var i = 2; i < num; i++) {
+	    if(num % i === 0) {
+	    	prime = false;
+	    	break;
+		}
+    }
+    return prime;
+}
+
+
+// getClosestToZero - a function which accepts arbitrary number of integer 
+// arguments and returns one closest to 0 (zero).
+// E.g. getClosestToZero(9, 5, -4, -9); // => -4
+// 	Tip: Math.abs() might be helpful
+
+function getClosestToZero () {
+    var min = Math.abs(arguments[0]);
+    var closestToZero = arguments[0];
+    for (let i = 1; i<arguments.length; i++) {
+        if ( Math.abs(arguments[i]) < min) {
+            min =  Math.abs(arguments[i]);
+            closestToZero = arguments[i];
+        }
+    }
+    return closestToZero;
+}
+
+console.log(getClosestToZero(9, 5, -4, -9));
--- 10_Functions/hw/task2/script.js
+++ 10_Functions/hw/task2/script.js
@@ -1,21 +1,72 @@
-// Написати конвертер валют.
-// За допомогою prompt користувач вводить кількість євро.
-// За допомогою prompt користувач вводить кількість доларів.
-// За допомогою alert вивести наступне:
-// 100 euros are equal 2900 UAH, 52 dollars are equal 1404UAH, one euro is equal 1.074 dollars.
-// Числа замінити на введені та обраховані дані
-
-var euros_rate = 29204;
-var dollars_rate = 25808;
-
-var euros_to_dollars_rate = euros_rate/dollars_rate;
-
-var euros = prompt('введить кількість євро');
-var dollars = prompt('введить кількість доларів');
-
-var euros_in_uah = euros*euros_rate/1000;
-
-var dollars_in_uah = dollars*dollars_rate/1000;
+var game = confirm ("Чи бажаєте почати гру?");
 
 
-alert(`${euros} euros are equal ${euros_in_uah.toFixed(2)} UAH, ${dollars} dollars are equal ${dollars_in_uah.toFixed(2)} UAH, one euro is equal ${euros_to_dollars_rate.toFixed(3)} dollars`);
+if (!game) {
+    console.log("Сьогодні ви не виграли мільйон, а могли");
+    alert("Сьогодні ви не виграли мільйон, а могли");
+} else {
+    var win_base_1 = 10;
+    var win_base_2 = 5;
+    var win_base_3 = 2;
+    var win_multiplayer = 1;
+
+    var range_base = 5;
+    var range_multiplayer = 1;
+
+    var num_of_attempts = 3;
+    var win_amount = 0;
+
+    outer: while (game) {
+     var number = Math.floor( Math.random() * (range_base * range_multiplayer + 1) );
+     console.log(`hint ;) - ${number}`); // for test purpose
+    
+
+        while (num_of_attempts--) {
+            var gamer_number = prompt(`Вгадайте число в проміжку від 0 до ${range_base*range_multiplayer}. Ваш виграш - ${win_amount}$. Можливо додатково виграти ${win_base_1 * win_multiplayer}$. Спроба №${3-num_of_attempts} з 3.`);
+         
+            if (gamer_number == number) {
+             
+                switch (num_of_attempts) {
+                    case 2:
+                    win_amount = win_amount + (win_base_1 * win_multiplayer);
+                    break;
+                    case 1:
+                    win_amount = win_amount + (win_base_2 * win_multiplayer);
+                    break;
+                    case 0:
+                    win_amount = win_amount + (win_base_3 * win_multiplayer);
+                    break;
+                    default:
+                    break;
+                }
+             
+                console.log(`Ваш виграш - ${win_amount}$`);
+                break;
+            } 
+            
+            // якщо це не перша гра (а гра на підвищення ставок - range_multiplayer != 1) та гравець програв (num_of_attempts === 0)
+            else if (num_of_attempts === 0 && range_multiplayer != 1) {
+                    win_amount = 0;
+                    console.log(`Дякуємо за гру, ваш виграш становить - ${win_amount}$`);
+                    alert(`Дякуємо за гру, ваш виграш становить - ${win_amount}$`);
+                    game = false;
+                    break outer;
+                }
+
+        }
+
+        game = confirm (`Ваш виграш - ${win_amount}$. Чи бажаєте продовжити гру?`);
+
+        if (!game) {
+            console.log(`Дякуємо за гру, ваш виграш становить - ${win_amount}$`);
+            alert(`Дякуємо за гру, ваш виграш становить - ${win_amount}$`);
+        } else if (range_multiplayer === 1 && win_amount === 0 ) { // якщо це перша гра та гравець програв, але хоче грати ще раз (game=true)
+                num_of_attempts = 3;
+                 // просто продовжуємо гру. не збільшуємо range_multiplayer, win_multiplayer
+        } else { // якщо гравець виграв та грає далі на підвищення
+            range_multiplayer = range_multiplayer * 2;
+            win_multiplayer = win_multiplayer * 3;
+            num_of_attempts = 3;
+        }
+    }
+}
