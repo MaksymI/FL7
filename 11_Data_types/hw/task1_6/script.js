50738a46a795c2ef025c2b851235db7ba4e7ae72
MaksymI <maksym.izmailov.lv@gmail.com>
2017-07-20 11:56:44 +0300

    L11 Homework done

--- 11_Data_types/hw/task1_6/script.js
+++ 11_Data_types/hw/task1_6/script.js
@@ -2,7 +2,6 @@
 // as arguments and runs that function per each element in array.
 // Also that function should be invoked with that array element.
 
-
 function forEach(arr, fn) {
     for (var item of arr) {
         fn(item);
@@ -12,11 +11,11 @@ function forEach(arr, fn) {
 forEach( [3, 5, 2], function(el){ console.log(el) } ); // -> 3 5 2
 
 
+
 // getTransformedArray - write function which takes array and function
 // as arguments and returns new array with every element being transformed by that function.
 // Note: reuse your forEach function, treat it as just an easy way to write your ordinary ‘for’
 // iterator, and the function you pass to it as just a body of that iterator.
-
 
 function getTransformedArray (arr, fn) {
     var output = [];
@@ -30,11 +29,11 @@ function increment(num){ return num + 1 };
 console.log(getTransformedArray( [1, 7, 20], increment )) // -> [ 2, 8, 21 ]
 
 
+
 // pluckByAttribute - write function which takes array of objects and label
 // of object attribute and returns array of values by that label.
 // Note: reuse your getTransformedArray function.
 // Tip: No need to create new array by yourself - getTransformedArray already does that
-
 
 function pluckByAttribute (arr, label) {
     return getTransformedArray (arr, function(element){
@@ -47,6 +46,7 @@ var presidents = [ { name: 'George', surname: 'Kush' } ,
 console.log(pluckByAttribute( presidents, 'name' )) // -> [‘George’, ‘Barako’]
 
 
+
 // cypherPhrase - write function which takes object and string and returns
 // string transformed by object rules.
 // Note: reuse your getTransformedArray function, don’t forget about 
@@ -54,7 +54,6 @@ console.log(pluckByAttribute( presidents, 'name' )) // -> [‘George’, ‘Bara
 // Tip: (falsy || ‘truthy’) returns ‘truthy’. e.g. var obj = { };
 // var letter = obj[‘a’] || ‘a’; // -> ‘a’, because obj[‘a’] would return ‘undefined’ which is falsy.
 // Tip: you can treat strings as just array of characters.
-
 
 function cypherPhrase (obj, str) {
     return getTransformedArray(str.split(''), function(element){
@@ -66,11 +65,11 @@ var charactersMap = {a: 'o', c: 'd', t: 'g'};
 console.log(cypherPhrase( charactersMap, 'kitty cat' )); // -> “kiggy dog”
 
 
+
 // decypherPhrase - write function which takes object and string
 // cyphered with cypherPhrase function and returns string back to normal.
 // Note: reuse your cypherPhrase function
 // Tip: easiest way to solve this task is to reverse key-value in charactersMap
-
 
 function decypherPhrase (obj, str) {
     var newObj = {};
@@ -85,6 +84,7 @@ function decypherPhrase (obj, str) {
 console.log(decypherPhrase( charactersMap, 'kiggy dog' )); // -> “kitty cat”
 
 
+
 // getTopNRichestNames - write function which takes number that indicates
 // how many names should function return in array and array of objects in a form of 
 // { name: ‘Bill Hatso’, income: ‘5B’ } where ‘B’ stands for billion. 
@@ -94,7 +94,6 @@ console.log(decypherPhrase( charactersMap, 'kiggy dog' )); // -> “kitty cat”
 // Note: use Array.prototype.sort, Array.prototype.map and your pluckByAttribute functions.
 // Tip: for your own convenience you can use 1e9 instead of 1000000000 which pretty much
 // means 10 to the power of 9 in JavaScript.
-
 
 function getTopNRichestNames(num, arr) {
     var abbreviations = {
