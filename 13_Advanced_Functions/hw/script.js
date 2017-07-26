fc759d83beaee846826c2f79c01c93a1ca0ff246
MaksymI <maksym.izmailov.lv@gmail.com>
2017-07-26 13:15:05 +0300

    compose done

--- 13_Advanced_Functions/hw/script.js
+++ 13_Advanced_Functions/hw/script.js
@@ -31,17 +31,22 @@ function smallest() {
     return Math.min.apply(null, arguments);
 }
 
+
+
 // Write `compose` function which performs right-to-left function composition.
 // Note: this function shouldn’t have limitaion on count of passed function
 // Invocation example:
-// const compose = require(‘./compose’);
+// const compose = require('./compose');
 // const toUpper = string => string.toUpperCase();
-// const classyGreeting = (firstName, lastName) =>
-// "The name's " + lastName + ", " + firstName + " " + lastName;
-// const yellGreeting = compose(
-//   toUpper,
-//   classyGreeting,
-// );
+// const classyGreeting = (firstName, lastName) => "The name's " + lastName + ", " + firstName + " " + lastName;
+// const yellGreeting = compose(toUpper, classyGreeting);
 // yellGreeting('James', 'Bond'); //=> "THE NAME'S BOND, JAMES BOND"
 
 
+function compose() {
+    return Array.prototype.reduceRight.call(arguments, function(prev, cur){
+        return function(){
+            return cur(prev.apply(null, arguments));
+        };   
+    });
+}
