78acfcaaf1af574b22c8369ff0a7a700a9b71ac7
MaksymI <maksym.izmailov.lv@gmail.com>
2017-08-08 21:24:22 +0300

    L15 Inheritance Homework Task1 done

--- 15_Inheritance/hw/task1/script.js
+++ 15_Inheritance/hw/task1/script.js
@@ -3,3 +3,29 @@
 // var defaults = { width: 100, height: 100 };
 // var options = { width: 150 };
 // var configs = assign({}, defaults, options); // -> {width: 150, height: 100}
+
+function assign(target) {
+
+    if (target === undefined || target === null) {
+        throw new TypeError('Cannot convert first argument to object');
+    }
+
+    var to = Object(target);
+
+    for (var i = 1; i < arguments.length; i++) {
+        var nextSource = arguments[i];
+        if (nextSource === undefined || nextSource === null) {
+          continue;
+        }
+
+        var keysArray = Object.keys(Object(nextSource));
+
+        for (var nextIndex = 0; nextIndex < keysArray.length; nextIndex++) {
+          var nextKey = keysArray[nextIndex];
+          to[nextKey] = nextSource[nextKey];
+        }
+    }
+
+    return to;
+}
+
