20c2b2fe975baac3893d34b59e53189bc8a5cedf
MaksymI <maksym.izmailov.lv@gmail.com>
2017-08-22 03:04:35 +0300

    storage.data creating if not exist, id genetration

--- 18_Networking/hw/server.js
+++ 18_Networking/hw/server.js
@@ -5,22 +5,39 @@ const fs = require('fs');
 
 // const storage = require('./storageManipulation');
 
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
+
+// const users = {"users": []};
+
+const createStorageFile = () => {
+    fs.readFile('storage.data', 'utf8', (err, data) => {
+        if(err && err.code == 'ENOENT') {
+            fs.appendFile('storage.data', '{"users": []}', err => {
+                if (err) throw err;
+                console.log('storage.data created just now!');
+              });
+        }
+    });
+}
+
+createStorageFile();
 
 const findUserByName = (obj, name) => (obj.users.filter(element => element.username == name));
 
-app.post('/api/users', function(req, res) {
+app.post('/api/users', (req, res) => {
     fs.readFile('storage.data', 'utf8', (err, data) => {
         if(err) {
             console.error(err.stack);
             res.status(500).send('Error on server!');
         } else {
             let obj = JSON.parse(data);
-            if (!(findUserByName(obj, req.body.username).length)) {
+            if (findUserByName(obj, req.body.username).length) {
                 res.status(409).send('user with given name already exist!');
             } else {
-                obj.users.push(req.body);
-                fs.writeFile('storage.data', JSON.stringify(obj), 'utf8', (err) => {
+                let toAdd = Object.assign({}, req.body);
+                toAdd.id = obj.users.length ? Math.max(...obj.users.map(e => e.id))+1 : 0;
+                obj.users.push(toAdd);
+                fs.writeFile('storage.data', JSON.stringify(obj), 'utf8', err => {
                     if (err) {
                         throw err
                     } else {
@@ -37,12 +54,8 @@ app.post('/api/users', function(req, res) {
 app.get('/api/users', (req, res) => {
     fs.readFile('storage.data', (err, data) => {
         if(err) {
-            // console.log(err);
             console.error(err.stack);
             res.status(500).send('Error on server!');
-            // res.statusCode = 500;
-            // res.end('Error on server!');
-            // return;
         }
         res.send(JSON.parse(data));
     });
@@ -51,7 +64,3 @@ app.get('/api/users', (req, res) => {
 const server = app.listen(3000, () => {
     console.log('listening on port ', server.address().port);
 });
-
-
-
-
