92ec06790c1ce045dab2369dd1c2c36a76f2d9a4
MaksymI <maksym.izmailov.lv@gmail.com>
2017-08-25 13:28:10 +0300

    Access-Control-Allow-Headers

--- 19_AJAX/hw/task2/index.js
+++ 19_AJAX/hw/task2/index.js
@@ -1,7 +1,10 @@
 const getJSON = (url, init) => fetch(url, init).then(response => response.json(), error => {throw error}); 
 
 const marsWeather = {};
-const getMarsWeather = getJSON('//marsweather.ingenology.com/v1/?format=json', {method: 'GET', mode: 'no-cors'});
+const getMarsWeather = getJSON('http://marsweather.ingenology.com/v1/', {headers:{
+    'Access-Control-Allow-Origin':'*',
+    "Access-Control-Allow-Headers": "Content-Type, Authorization"}
+});
 
 getMarsWeather.then(data => {
     console.log(data);
@@ -9,3 +12,64 @@ getMarsWeather.then(data => {
     marsWeather.latest = data.latest;
 }, error => {console.log(error);});
 
+
+// fetch('http://marsweather.ingenology.com/v1/?format=json', {mode: 'no-cors'})
+//     .then(function(response) {
+//         return response.arrayBuffer();
+//     }, error => {throw error})
+//     .then(buffer => {
+//         console.log(buffer);
+//         // marsWeather.archive = data.archive;
+//         // marsWeather.latest = data.latest;
+//     }, error => {console.log(error)})
+
+
+// var brokenJson = function (url, init) {
+//     var responseCopy;
+//     return fetch(url, init)
+//     .then(function (response) {
+//         responseCopy = response.clone();
+//         return response.json();
+//     }).catch(function (err) {
+//         if (err instanceof SyntaxError) {
+//             return responseCopy.text()
+//             .then(function(text) {
+//                 console.error(text);
+//                 throw err;
+//             });
+//         }
+//         else {
+//             throw err;
+//         }
+//     }).then(function (json) {
+//         console.log(json);
+//         marsWeather.archive = json.archive;
+//         marsWeather.latest = json.latest;
+//     });
+// };
+
+// brokenJson('http://marsweather.ingenology.com/v1/?format=json', {mode: 'no-cors'});
+
+// var res = fetch('http://marsweather.ingenology.com/v1/?format=json', {mode: 'no-cors'}).then(function(response) {
+//     return response.clone().json().catch(function() {
+//       return response.text();
+//     });
+//   });
+
+// res.then(data => console.log(data));
+
+// function streamingRead() {
+//     var res;
+//     var req = new Request('http://marsweather.ingenology.com/v1/?format=json', {mode: 'no-cors'});
+//     fetch(req).then(function(response) {
+//       var reader = response.body.getReader();
+//       return reader.read();
+//     }).then(function(result, done) {
+//       if (!done) {
+//           res+=result;
+//           console.log(res);
+//         // do something with each chunk
+//       }
+//     });
+//   }
+//   streamingRead();
