f6805189b2c5ef13a77c3b0169fee8320bcd3206
MaksymI <maksym.izmailov.lv@gmail.com>
2017-08-25 22:33:10 +0300

    jsonp method done

--- 19_AJAX/hw/task2/jsonp.js
+++ 19_AJAX/hw/task2/jsonp.js
@@ -1,4 +1,3 @@
-const marsWeatherLatest = {};
 const marsWeatherArchive = {results: []};
 
 const roundOverage = (a, b) => {
@@ -6,15 +5,9 @@ const roundOverage = (a, b) => {
     return res.toFixed(1);
 };
 
-const latestHandler = data => {
-    marsWeatherLatest.data = data.report.terrestrial_date;
-    marsWeatherLatest.temp = roundOverage(data.report.min_temp, data.report.max_temp);
-    marsWeatherLatest.maxTemp = data.report.max_temp;
-    marsWeatherLatest.windSpeed = data.report.wind_speed;
-    marsWeatherLatest.windDirection = data.report.wind_direction;
-};
 
 const archiveHandler = data => {
+    // console.log(data.next);
     marsWeatherArchive.next = data.next;
     marsWeatherArchive.previous = data.previous;
     data.results.forEach(element => {marsWeatherArchive.results.push({
@@ -28,7 +21,6 @@ const archiveHandler = data => {
     });
 };
 
-const latestUrl = 'http://marsweather.ingenology.com/v1/latest/?format=jsonp&callback=latestHandler';
 const archiveUrl = 'http://marsweather.ingenology.com/v1/archive/?format=jsonp&callback=archiveHandler';
 
 const getJSONP = src => {
@@ -38,9 +30,54 @@ const getJSONP = src => {
     setTimeout(() => {script.parentNode.removeChild(script)}, 0);
 }
   
-getJSONP(latestUrl);
 getJSONP(archiveUrl);
 
+const data = document.getElementById('data');
+
+const temperature = document.getElementById('temperature');
+
+const windDirection = document.getElementById('wind_dir');
+
+const windPower = document.getElementById('wind_power');
+
+const nextButton = document.getElementById('next');
+
+const prevButton = document.getElementById('prev');
+
+
+prevButton.addEventListener('click', function() {
+    ++renderArchive.clickCounter;
+    renderArchive();
+    if ((renderArchive.clickCounter+1)%10 === 0) {
+        getJSONP(marsWeatherArchive.next);
+    }
+});
+
+nextButton.addEventListener('click', function() {
+    --renderArchive.clickCounter;
+    if (renderArchive.clickCounter < 0) {
+        renderArchive.clickCounter = -1;   
+    }
+    renderArchive();
+});
+
+const renderArchive = () => {
+    if (renderArchive.clickCounter < 0) {
+        renderArchive.clickCounter = 0;
+    }
+    data.innerText = marsWeatherArchive.results[renderArchive.clickCounter].data;
+    temperature.innerText = marsWeatherArchive.results[renderArchive.clickCounter].temp + " \u00B0C";
+    windDirection.innerText = marsWeatherArchive.results[renderArchive.clickCounter].windDirection || 'no data';
+    windPower.innerText = marsWeatherArchive.results[renderArchive.clickCounter].windSpeed || 'no data';
+}
+
+renderArchive.clickCounter = 0;
+
+window.addEventListener("hashchange", renderArchive);
+
+setTimeout(function(){location.hash='start';}, 500);
+
+// document.addEventListener("onload", renderLoad);
 
 
 // getJSONP(marsWeatherArchive.next);
