5cf08434fb37654478c9b468933b25ff4bbd2ec4
MaksymI <maksym.izmailov.lv@gmail.com>
2017-08-28 22:44:54 +0300

    finding white element

--- 20_jQuery/hw/js/script.js
+++ 20_jQuery/hw/js/script.js
@@ -1,4 +1,38 @@
 $(function() {
+
+    var timer;
+    var start;
+    var steps = 0;
+    var pointFrom;
+    var pointTo;
+
+    $('.grid').click(function(event) {
+        steps++;
+        // console.log(event);
+       
+        if (event.target.children.length === 0) {
+            pointFrom = event.target.parentNode;
+        } else {
+            pointFrom = event.target;
+        }
+        if (pointFrom.previousElementSibling && pointFrom.previousElementSibling.id == 'white') {
+            pointTo = pointFrom.previousElementSibling;
+        } else if(pointFrom.nextElementSibling && pointFrom.nextElementSibling.id == 'white') {
+            pointTo = pointFrom.nextElementSibling;
+        } else if(pointFrom.nextElementSibling && pointFrom.nextElementSibling.nextElementSibling && pointFrom.nextElementSibling.nextElementSibling.nextElementSibling && pointFrom.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling && pointFrom.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.id == 'white') {
+            pointTo = pointFrom.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling;
+        } else if(pointFrom.previousElementSibling && pointFrom.previousElementSibling.previousElementSibling && pointFrom.previousElementSibling.previousElementSibling.previousElementSibling && pointFrom.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling && pointFrom.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.id == 'white') {
+            pointTo = pointFrom.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling;
+        } 
+        console.log(pointTo);
+
+        $(pointFrom).fadeOut("slow", function(){
+            // $(this).replaceWith(pointTo);
+            $(pointFrom).fadeIn("slow");
+        });
+
+      });
+      
     
     $('.new-game').on("click", newGame);
     $('.win').on("click", win);
@@ -12,13 +46,13 @@ $(function() {
         for (var i=0; i < k; i++) {
             newArr.push($(arr[i]).clone()[0]);
         }
-        // while (--k) {
-        //     var j = Math.floor(Math.random() * (k + 1)),
-        //         tempk = newArr[k],
-        //         tempj = newArr[j];
-        //         newArr[k] = tempj;
-        //         newArr[j] = tempk;
-        // }
+        while (--k) {
+            var j = Math.floor(Math.random() * (k + 1)),
+                tempk = newArr[k],
+                tempj = newArr[j];
+                newArr[k] = tempj;
+                newArr[j] = tempk;
+        }
         return newArr.reverse();
     }
 
@@ -51,26 +85,39 @@ $(function() {
     //     $(this).replaceWith(newItems[4]);
     //     $(items[4]).fadeIn("slow");
     //  });
+
+    
    
     function newGame() {
+                
         for (let i=0; i < items.length; i++) {
             $(items[i]).fadeOut("slow", function(){
-                // var div = $("<div id='foo'>test2</div>").hide();
                 $(this).replaceWith(newItems[i]);
                 $(items[i]).fadeIn("slow");
             });
         }
+
+        start = new Date;
+        
+        timer = setInterval(function() {
+            $('.clock').text(parseInt((new Date - start) / 1000));
+            $('.steps-num').text(steps);
+        }, 500);
+        
     }
 
     function win() {
-        var currentItems = $(".grid").children().toArray();
-        for (let i=0; i < items.length; i++) {
-            $(currentItems[i]).fadeOut("slow", function(){
-                // var div = $("<div id='foo'>test2</div>").hide();
-                $(this).replaceWith(items[i]);
-                $(currentItems[i]).fadeIn("slow");
-            });
-        }
+        location.reload();
+        // clearInterval(timer);
+        // $('.clock').text('0');
+        // start = new Date;
+        // var currentItems = $(".grid").children().toArray();
+        // for (let i=0; i < items.length; i++) {
+        //     $(currentItems[i]).fadeOut("slow", function(){
+        //         $(this).replaceWith(items[i]);
+        //         $(currentItems[i]).fadeIn("slow");
+        //     });
+        // }
     }
 
 });
--- 20_jQuery/hw/tasks/commonJs.js
+++ 20_jQuery/hw/tasks/commonJs.js
@@ -1,14 +1,14 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
-// var browserSync = require('browser-sync');
+var browserSync = require('browser-sync');
 
 gulp.task('commonJs', function() {
 	return gulp.src([
 		'./js/script.js'
 		])
 	.pipe(concat('script.min.js'))
-	.pipe(uglify())
-	.pipe(gulp.dest('./js'));
-	// .pipe(browserSync.reload({stream: true}));
+	// .pipe(uglify())
+	.pipe(gulp.dest('./js'))
+	.pipe(browserSync.reload({stream: true}));
 });
