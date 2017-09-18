26b476ad41de611f2ff3ba431211aecdc4242dc6
MaksymI <maksym.izmailov.lv@gmail.com>
2017-09-18 22:07:49 +0300

    Angular CDN

--- 16_ES_Next/hw/modules_default_export_math.js
+++ 16_ES_Next/hw/modules_default_export_math.js
@@ -1,6 +1,6 @@
 export const PI = 3.141592;
-    
-export const _sqrt = (s, x, last) =>(x != last ? _sqrt(s, (x + s / x) / 2.0, x) : x);
+
+const _sqrt = (s, x, last) =>(x != last ? _sqrt(s, (x + s / x) / 2.0, x) : x);
 
 export const sqrt = s => _sqrt(s, s/2.0, 0.0);
 
